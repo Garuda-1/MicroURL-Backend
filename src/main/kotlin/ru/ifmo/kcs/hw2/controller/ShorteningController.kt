e680bfef63fc5c5660e39669e8573f1446c22cc8
Ian <yan.dolganskiy@mail.ru>
2020-12-30 15:29:15 +0300

    Help(

--- src/main/kotlin/ru/ifmo/kcs/hw2/controller/ShorteningController.kt
+++ src/main/kotlin/ru/ifmo/kcs/hw2/controller/ShorteningController.kt
@@ -1,7 +1,8 @@
 package ru.ifmo.kcs.hw2.controller
 
+import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.MediaType
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import ru.ifmo.kcs.hw2.model.UrlMapping
 import ru.ifmo.kcs.hw2.service.UrlMappingService
@@ -9,31 +10,38 @@ import ru.ifmo.kcs.hw2.service.UrlShorteningService
 import javax.servlet.http.HttpServletResponse
 
 @RestController
+@RequestMapping("/url")
 class ShorteningController {
     class ShorteningDto(val originalUrl: String)
 
-    @Autowired
-    private lateinit var urlShorteningService: UrlShorteningService
+//    @Autowired
+//    private lateinit var urlShorteningService: UrlShorteningService
 
-    @Autowired
-    private lateinit var urlMappingService: UrlMappingService
+//    @Autowired
+//    private lateinit var urlMappingService: UrlMappingService
 
-    @PostMapping(value=["/url/generate"], produces=[MediaType.APPLICATION_JSON_VALUE])
+    @PostMapping("/generate")
     fun generateLink(@RequestBody requestBody: ShorteningDto): UrlMapping {
-        val originalUrl = requestBody.originalUrl
-        val shortUrl = urlShorteningService.shortenUrl(originalUrl)
-        val urlMapping = UrlMapping(shortUrl, originalUrl)
-        urlMappingService.saveNewMapping(urlMapping)
-        return urlMapping
+//        val originalUrl = requestBody.originalUrl
+//        val shortUrl = urlShorteningService.shortenUrl(originalUrl)
+//        val urlMapping = UrlMapping(shortUrl, originalUrl)
+//        urlMappingService.saveNewMapping(urlMapping)
+//        return urlMapping
+        return UrlMapping("a", "b")
     }
 
-    @GetMapping(value=["/urls/{short}"])
+    @GetMapping("/short/{short}")
     fun getUrlMapping(@PathVariable(value="short") shortUrl: String, response: HttpServletResponse) {
-        val urlMapping = urlMappingService.getByShortUrl(shortUrl)
-        if (urlMapping == null) {
-            response.sendError(400)
-        } else {
-            response.sendRedirect(urlMapping.originalUrl)
-        }
+//        val urlMapping = urlMappingService.getByShortUrl(shortUrl)
+//        if (urlMapping == null) {
+//            response.sendError(400)
+//        } else {
+//            response.sendRedirect(urlMapping.originalUrl)
+//        }
+    }
+
+    @GetMapping("/")
+    fun getRoot(): String {
+        return "Help"
     }
 }
