cea78b1f9e0e82dd3da64ae126c35b16f0eff697
Ian <yan.dolganskiy@mail.ru>
2021-01-22 21:41:59 +0300

    Fixed URL encoding algorithm

--- src/main/kotlin/ru/ifmo/kcs/hw2/service/UrlShorteningService.kt
+++ src/main/kotlin/ru/ifmo/kcs/hw2/service/UrlShorteningService.kt
@@ -8,7 +8,7 @@ import ru.ifmo.kcs.hw2.model.Range
 @Service
 class UrlShorteningService {
     private val arrCons: Array<Char> = arrayOf('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z',
-            'B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9')
+            'B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z')
     private val arrVow: Array<Char> = arrayOf('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
     private val shortLinkSize = 6
 
